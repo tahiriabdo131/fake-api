bd261f0fc6e988d16f269ae90445247b4bf825cc
Abdelali TAHIRI <tahiriabdo131@gmail.com>
2023-01-27 20:43:10 +0100

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-const router = jsonServer.router('/tmp/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
